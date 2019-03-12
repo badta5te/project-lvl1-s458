845f5bc46c8e51a72368fdd3c0c137efa4e2c6a4
Pavel <mikhailov.sztu@gmail.com>
2019-03-12 20:30:49 +0300

    refactor brain-even.php

--- src/brain-even.php
+++ src/brain-even.php
@@ -5,46 +5,40 @@ namespace BrainGames\BrainEven;
 use function cli\line;
 use function cli\prompt;
 
-const NUMBER_OF_GAMES = 3;
-const MIN_NUMBER = 1;
-const MAX_NUMBER = 100;
-
+const GREETING = "Welcome to the Brain Game!";
+const GAME_RULES = "Answer 'yes' if number even otherwise answer 'no'.\n";
+const COUNT_OF_ROUNDS = 3;
 
 function isEven($number)
 {
     return $number % 2 === 0;
 }
 
-function isEvenGameNumber($number)
-{
-    return isEven($number) ? 'yes' : 'no';
-}
-
 function game()
 {
-    line('Welcome to the Brain Game!');
-    line("Answer 'yes' if number even otherwise answer 'no'.\n");
+    line(GREETING);
+    line(GAME_RULES);
     $name = prompt('May I have your name?');
     line('Hello, %s!', $name);
 
-    $rightAnswers = 0;
+    $gamesPlayed = 0;
 
-    for ($game = 1; $game <= NUMBER_OF_GAMES; $game++) {
-        $number = rand(MIN_NUMBER, MAX_NUMBER);
+    for ($i = 1; $i <= COUNT_OF_ROUNDS; $i++) {
+        $number = rand(1, 100);
+        $parityOfNumber = isEven($number) ? 'yes' : 'no';
         line('Question: %s', $number);
         $answer = prompt('Your answer:');
-        $gameResult = isEvenGameNumber($number);
-        if ($gameResult === $answer) {
+        if ($parityOfNumber === $answer) {
             line('Correct!');
-            $rightAnswers++;
+            $gamesPlayed++;
         } else {
             break;
         }
     }
-    if ($rightAnswers == 3) {
+    if ($gamesPlayed == 3) {
         line('Congratulations, %s!', $name);
     } else {
-        line("'%s', is wrong answer ;(. Correct answer was '%s'\n", $answer, $gameResult);
+        line("'%s', is wrong answer ;(. Correct answer was '%s'\n", $answer, $parityOfNumber);
         line("Let's try again, %s!", $name);
     }
 }
