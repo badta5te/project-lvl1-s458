e444ce472aafdf4340b954bcf27d077a805237c9
Pavel <mikhailov.sztu@gmail.com>
2019-03-14 22:54:23 +0300

    fix progression.php

--- src/games/progression.php
+++ src/games/progression.php
@@ -12,7 +12,7 @@ function game()
     $data = function () {
         $begin = rand(1, 5);
         $step = rand(1, 5);
-        $hiddenElement = rand(0, SIZE_OF_PROGRESSION - 1);
+        $positionOfHiddenElement = rand(0, SIZE_OF_PROGRESSION - 1);
         $progression = [];
         $currentElement = $beginOfProgression;
         for ($i = 0; $i < SIZE_OF_PROGRESSION; $i++) {
